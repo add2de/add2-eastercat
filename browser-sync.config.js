37ce00d0b2163dbd3bef089356b857199dd64279
pr <pr@add2.de>
2017-10-20 17:54:50 +0200

    Babel, Rollup und Browser Sync konfiguriert

--- browser-sync.config.js
+++ browser-sync.config.js
@@ -13,97 +13,99 @@
  |
  */
 module.exports = {
-    "ui": {
-        "port": 3001,
-        "weinre": {
-            "port": 8080
-        }
+  ui: {
+    port: 3001,
+    weinre: {
+      port: 8080,
     },
-    "files": false,
-    "watchEvents": [
-        "change"
-    ],
-    "watchOptions": {
-        "ignoreInitial": true
+  },
+  files: {
+    files: ['./dist/**/*js'],
+  },
+  watchEvents: [
+    'change',
+  ],
+  watchOptions: {
+    ignoreInitial: true,
+  },
+  server: {
+    baseDir: ['dist', 'src'],
+  },
+  proxy: false,
+  port: 9000,
+  middleware: false,
+  serveStatic: [],
+  ghostMode: {
+    clicks: true,
+    scroll: true,
+    location: true,
+    forms: {
+      submit: true,
+      inputs: true,
+      toggles: true,
     },
-    "server":  {
-      baseDir: ['src'],
+  },
+  logLevel: 'info',
+  logPrefix: 'Browsersync',
+  logConnections: false,
+  logFileChanges: true,
+  logSnippet: true,
+  rewriteRules: [],
+  open: 'local',
+  browser: 'default',
+  cors: false,
+  xip: false,
+  hostnameSuffix: false,
+  reloadOnRestart: false,
+  notify: false,
+  scrollProportionally: true,
+  scrollThrottle: 0,
+  scrollRestoreTechnique: 'window.name',
+  scrollElements: [],
+  scrollElementMapping: [],
+  reloadDelay: 0,
+  reloadDebounce: 0,
+  reloadThrottle: 0,
+  plugins: [],
+  injectChanges: true,
+  startPath: null,
+  minify: true,
+  host: null,
+  localOnly: false,
+  codeSync: true,
+  timestamps: true,
+  clientEvents: [
+    'scroll',
+    'scroll:element',
+    'input:text',
+    'input:toggles',
+    'form:submit',
+    'form:reset',
+    'click',
+  ],
+  socket: {
+    socketIoOptions: {
+      log: false,
     },
-    "proxy": false,
-    "port": 9000,
-    "middleware": false,
-    "serveStatic": [],
-    "ghostMode": {
-        "clicks": true,
-        "scroll": true,
-        "location": true,
-        "forms": {
-            "submit": true,
-            "inputs": true,
-            "toggles": true
-        }
+    socketIoClientConfig: {
+      reconnectionAttempts: 50,
     },
-    "logLevel": "info",
-    "logPrefix": "Browsersync",
-    "logConnections": false,
-    "logFileChanges": true,
-    "logSnippet": true,
-    "rewriteRules": [],
-    "open": "local",
-    "browser": "default",
-    "cors": false,
-    "xip": false,
-    "hostnameSuffix": false,
-    "reloadOnRestart": false,
-    "notify": false,
-    "scrollProportionally": true,
-    "scrollThrottle": 0,
-    "scrollRestoreTechnique": "window.name",
-    "scrollElements": [],
-    "scrollElementMapping": [],
-    "reloadDelay": 0,
-    "reloadDebounce": 0,
-    "reloadThrottle": 0,
-    "plugins": [],
-    "injectChanges": true,
-    "startPath": null,
-    "minify": true,
-    "host": null,
-    "localOnly": false,
-    "codeSync": true,
-    "timestamps": true,
-    "clientEvents": [
-        "scroll",
-        "scroll:element",
-        "input:text",
-        "input:toggles",
-        "form:submit",
-        "form:reset",
-        "click"
-    ],
-    "socket": {
-        "socketIoOptions": {
-            "log": false
-        },
-        "socketIoClientConfig": {
-            "reconnectionAttempts": 50
-        },
-        "path": "/browser-sync/socket.io",
-        "clientPath": "/browser-sync",
-        "namespace": "/browser-sync",
-        "clients": {
-            "heartbeatTimeout": 5000
-        }
+    path: '/browser-sync/socket.io',
+    clientPath: '/browser-sync',
+    namespace: '/browser-sync',
+    clients: {
+      heartbeatTimeout: 5000,
     },
-    "tagNames": {
-        "less": "link",
-        "scss": "link",
-        "css": "link",
-        "jpg": "img",
-        "jpeg": "img",
-        "png": "img",
-        "svg": "img",
-        "gif": "img",
-        "js": "script"
-    }
+  },
+  tagNames: {
+    less: 'link',
+    scss: 'link',
+    css: 'link',
+    jpg: 'img',
+    jpeg: 'img',
+    png: 'img',
+    svg: 'img',
+    gif: 'img',
+    js: 'script',
+  },
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
 export default {
   input: 'src/add2kat.es6',
   output: {
     file: 'dist/add2kat.js',
-    format: 'cjs',
+    format: 'iife',
   },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
 };
