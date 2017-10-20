96899c5c565c1acf1a0f04363f4d6ad8c2f13b68
pr <pr@add2.de>
2017-10-20 18:36:05 +0200

    rollup umsortiert

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,14 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/add2kat.es6',
-  output: {
-    file: 'dist/add2kat.js',
-    format: 'iife',
-  },
   plugins: [
     resolve(),
     babel({
       exclude: 'node_modules/**',
     }),
   ],
+  output: {
+    file: 'dist/add2kat.js',
+    format: 'iife',
+  },
 };
